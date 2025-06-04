bae364d74555bd86045d7449fcfb12eb15094886
chris <ivi3001@yahoo.de>
2025-06-04 14:57:12 +0200

    backup

--- src/PlotApp.cpp
+++ src/PlotApp.cpp
@@ -1,4 +1,5 @@
-#include "PlotApp.h"
+#include "PlotApp.hpp"
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <imgui.h>
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,36 @@
-#include "PlotApp.h"
-#include <math.h>
+#include "PlotApp.hpp"
 
-// #include <imgui.h>
-// #include <implot.h>
-// #include "nanovg.h"
+#include <math.h>
+#include <imgui.h>
+#include <implot.h>
+#include "nanovg.h"
+
+struct PaintState {
+    float x = 0.0f;
+    float speed = 1.0f;
+
+    void update() {
+        x += speed;
+        if (x > 300.0f) x = 0.0f;
+    }
+
+    void draw(NVGcontext* vg) {
+        update();
+
+        nvgBeginPath(vg);
+        nvgCircle(vg, x, 100, 40);
+        nvgFillColor(vg, nvgRGBA(255, 100, 0, 255));
+        nvgFill(vg);
+    }
+};
+
 int main() {
     PlotApp::Init(1280, 720);
 
-    PlotApp::SetImGuiCallback([](){
+    // Basic ImPlot window
+    PlotApp::SetImGuiCallback([]() {
         ImGui::Begin("Sine Plot");
-        if (ImPlot::BeginPlot("Sine")) {
+        if (ImPlot::BeginPlot("Sine Wave")) {
             static float x[100], y[100];
             for (int i = 0; i < 100; ++i) {
                 x[i] = i * 0.1f;
@@ -21,11 +42,19 @@ int main() {
         ImGui::End();
     });
 
-    PlotApp::AddPaintWindow("NanoVG Window", [](NVGcontext* vg){
-        nvgBeginPath(vg);
-        nvgCircle(vg, 100, 100, 50);
-        nvgFillColor(vg, nvgRGBA(255, 0, 0, 255));
-        nvgFill(vg);
+    // NanoVG paint window with persistent state
+    static PaintState paint;
+
+    // Add an ImGui UI to control paint state
+    PlotApp::SetImGuiCallback([]() {
+        ImGui::Begin("Control Panel");
+        ImGui::SliderFloat("Speed", &paint.speed, 0.1f, 10.0f);
+        ImGui::End();
+    });
+
+    // Register the paint window using state
+    PlotApp::AddPaintWindow("NanoVG Canvas", [](NVGcontext* vg) {
+        paint.draw(vg);
     });
 
     PlotApp::Run();
