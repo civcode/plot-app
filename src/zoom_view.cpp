a40d7317befd71c35848aeef35a6a7c7f6a06da3
chris <ivi3001@yahoo.de>
2025-06-05 11:42:37 +0200

    zoom works now

--- src/zoom_view.cpp
+++ src/zoom_view.cpp
@@ -10,15 +10,66 @@ struct State {
 
 static std::unordered_map<std::string, State> stateMap;
 
+// void Draw(const std::string& label, NVGcontext* vg, std::function<void(NVGcontext*)> drawCallback) {
+//     State& state = stateMap[label];
+
+//     ImVec2 canvasPos = ImGui::GetCursorScreenPos();
+//     ImVec2 canvasSize = ImGui::GetContentRegionAvail();
+
+//     // ImGui::InvisibleButton(("##" + label).c_str(), canvasSize,
+//     //     ImGuiButtonFlags_MouseButtonLeft | ImGuiButtonFlags_MouseButtonRight);
+
+//     bool hovering = ImGui::IsItemHovered();
+//     ImVec2 mouse = ImGui::GetIO().MousePos;
+//     ImVec2 delta = ImGui::GetIO().MouseDelta;
+
+//     if (hovering) {
+//         float wheel = ImGui::GetIO().MouseWheel;
+//         if (wheel != 0.0f) {
+//             float zoomFactor = 1.1f;
+//             float prevZoom = state.zoom;
+//             state.zoom *= (wheel > 0) ? zoomFactor : (1.0f / zoomFactor);
+//             ImVec2 mouseInCanvas = ImVec2(mouse.x - canvasPos.x, mouse.y - canvasPos.y);
+//             state.offset.x -= mouseInCanvas.x * (state.zoom - prevZoom) / state.zoom;
+//             state.offset.y -= mouseInCanvas.y * (state.zoom - prevZoom) / state.zoom;
+//         }
+//     }
+
+//     if (hovering && ImGui::IsMouseDragging(ImGuiMouseButton_Right)) {
+//         state.offset.x += delta.x;
+//         state.offset.y += delta.y;
+//     }
+
+//     // Actual rendering with zoom and pan
+//     // nvgBeginFrame(vg, canvasSize.x, canvasSize.y, 1.0f);
+//     nvgSave(vg);
+//     nvgTranslate(vg, state.offset.x, state.offset.y);
+//     nvgScale(vg, state.zoom, state.zoom);
+//     drawCallback(vg);
+//     nvgRestore(vg);
+//     // nvgEndFrame(vg);
+// }
+
 void Draw(const std::string& label, NVGcontext* vg, std::function<void(NVGcontext*)> drawCallback) {
     State& state = stateMap[label];
 
     ImVec2 canvasPos = ImGui::GetCursorScreenPos();
     ImVec2 canvasSize = ImGui::GetContentRegionAvail();
+    if (canvasSize.x <= 0.0f || canvasSize.y <= 0.0f)
+        return;
 
-    // ImGui::InvisibleButton(("##" + label).c_str(), canvasSize,
-    //     ImGuiButtonFlags_MouseButtonLeft | ImGuiButtonFlags_MouseButtonRight);
+    // Save current ImGui cursor position
+    ImVec2 cursorBackup = ImGui::GetCursorPos();
 
+    // Set screen position and create an invisible button for input handling
+    ImGui::SetCursorScreenPos(canvasPos);
+    ImGui::InvisibleButton(("##ZoomBtn_" + label).c_str(), canvasSize,
+        ImGuiButtonFlags_MouseButtonLeft | ImGuiButtonFlags_MouseButtonRight);
+
+    // Restore cursor position so layout isn't broken
+    ImGui::SetCursorPos(cursorBackup);
+
+    // Handle interaction
     bool hovering = ImGui::IsItemHovered();
     ImVec2 mouse = ImGui::GetIO().MousePos;
     ImVec2 delta = ImGui::GetIO().MouseDelta;
@@ -33,21 +84,19 @@ void Draw(const std::string& label, NVGcontext* vg, std::function<void(NVGcontex
             state.offset.x -= mouseInCanvas.x * (state.zoom - prevZoom) / state.zoom;
             state.offset.y -= mouseInCanvas.y * (state.zoom - prevZoom) / state.zoom;
         }
+
+        if (ImGui::IsMouseDragging(ImGuiMouseButton_Right)) {
+            state.offset.x += delta.x;
+            state.offset.y += delta.y;
+        }
     }
 
-    if (hovering && ImGui::IsMouseDragging(ImGuiMouseButton_Right)) {
-        state.offset.x += delta.x;
-        state.offset.y += delta.y;
-    }
-
-    // Actual rendering with zoom and pan
-    // nvgBeginFrame(vg, canvasSize.x, canvasSize.y, 1.0f);
+    // Apply zoom/pan transform to NanoVG
     nvgSave(vg);
     nvgTranslate(vg, state.offset.x, state.offset.y);
     nvgScale(vg, state.zoom, state.zoom);
     drawCallback(vg);
     nvgRestore(vg);
-    // nvgEndFrame(vg);
 }
 
 } // namespace ZoomView
