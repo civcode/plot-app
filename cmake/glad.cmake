1dd9966fba66c581a09e4c87c0caaffbe58c1302
chris <ivi3001@yahoo.de>
2025-06-09 16:23:36 +0200

    added example build

--- cmake/glad.cmake
+++ cmake/glad.cmake
@@ -1,14 +1,9 @@
-# cmake/glad.cmake
-
 include(FetchContent)
 
-# Declare the glad dependency
 FetchContent_Declare(
     glad
     GIT_REPOSITORY https://github.com/Dav1dde/glad.git
     GIT_TAG        v0.1.34
 )
-
-# Ensure glad is downloaded and added to the build
 FetchContent_MakeAvailable(glad)
-set_target_properties(glad PROPERTIES POSITION_INDEPENDENT_CODE ON)
+# set_target_properties(glad PROPERTIES POSITION_INDEPENDENT_CODE ON)
