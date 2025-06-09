c968c06e4838a22b033f9ec72fb85797b4024697
chris <ivi3001@yahoo.de>
2025-06-09 16:56:26 +0200

    cleaned up cmake file

--- cmake/glad.cmake
+++
@@ -1,9 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(
-    glad
-    GIT_REPOSITORY https://github.com/Dav1dde/glad.git
-    GIT_TAG        v0.1.34
-)
-FetchContent_MakeAvailable(glad)
-# set_target_properties(glad PROPERTIES POSITION_INDEPENDENT_CODE ON)
